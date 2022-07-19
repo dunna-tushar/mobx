4891546a4d39e1fad5418fb9ccefeef2c00d85d1
dunna-tushar <dunnatushar6@gmail.com>
2022-07-19 17:27:02 +0530

    movies setup

--- src/pages/Movies/index.jsx
+++ src/pages/Movies/index.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Genres from "../../components/Genres";
+import Singlecontent from "../../components/SingleContent";
+import Pagination from "../../components/pagination";
+import { REACT_APP_API_KEY } from "../../config";
+import useGenre from "../../hooks/useGenre";
 
 const Movies = () => {
-  return (
-    <div>Movies</div>
-  )
-}
+  const [genres, setGenres] = useState([]);
+  const [selectedGenres, setSelectedGenres] = useState([]);
+  const [page, setPage] = useState(1);
+  const [content, setContent] = useState([]);
+  const [numOfPages, setNumOfPages] = useState();
+  const genreforURL = useGenre(selectedGenres);
 
-export default Movies
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `https://api.themoviedb.org/3/discover/movie?api_key=${REACT_APP_API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page}&with_genres=${genreforURL}`
+    );
+    setContent(data.results);
+    setNumOfPages(data.total_pages);
+  };
+
+  useEffect(() => {
+    window.scroll(0, 0);
+    fetchMovies();
+  }, [genreforURL, page]);
+
+  return (
+    <div>
+      <span className="pageTitle">Discover Movies</span>
+      <Genres
+        type="movie"
+        selectedGenres={selectedGenres}
+        setSelectedGenres={setSelectedGenres}
+        genres={genres}
+        setGenres={setGenres}
+        setPage={setPage}
+      />
+      <div className="trending">
+        {content &&
+          content.map((c) => (
+            <Singlecontent
+              key={c.id}
+              id={c.id}
+              poster={c.poster_path}
+              title={c.title || c.name}
+              date={c.first_air_date || c.release_date}
+              media_type="movie"
+              vote_average={c.vote_average}
+            />
+          ))}
+      </div>
+      {numOfPages > 1 && (
+        <Pagination setPage={setPage} numOfPages={numOfPages} />
+      )}
+    </div>
+  );
+};
+
+export default Movies;
